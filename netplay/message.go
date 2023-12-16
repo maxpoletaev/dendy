58af9f154371bf202539dc85706ce164213b0ac9
Max Poletaev <max.poletaev@gmail.com>
2023-12-16 22:31:19 +0100

    Message decode cleanups

--- netplay/message.go
+++ netplay/message.go
@@ -9,7 +9,7 @@ import (
 	"net"
 )
 
-type MsgType uint8
+type MsgType = uint8
 
 const (
 	MsgTypeReset MsgType = iota + 1
@@ -28,39 +28,46 @@ type Message struct {
 func (m *Message) Encode() ([]byte, error) {
 	var buf bytes.Buffer
 
-	buf.Write([]byte{byte(m.Type)})
+	buf.Write([]byte{m.Type})
 	err1 := binary.Write(&buf, binary.LittleEndian, m.Frame)
 	err2 := binary.Write(&buf, binary.LittleEndian, m.Generation)
 	err3 := binary.Write(&buf, binary.LittleEndian, uint32(len(m.Payload)))
 
 	if err := errors.Join(err1, err2, err3); err != nil {
-		return nil, fmt.Errorf("failed to encode message header: %v", err)
+		return nil, err
 	}
 
 	if _, err := buf.Write(m.Payload); err != nil {
-		return nil, fmt.Errorf("failed to encode message payload: %v", err)
+		return nil, err
 	}
 
 	return buf.Bytes(), nil
 }
 
 func (m *Message) Decode(data []byte) error {
-	buf := bytes.NewBuffer(data)
-	m.Type = MsgType(buf.Next(1)[0])
+	var (
+		dataSize uint32
+		err      error
+	)
 
-	var payloadSize uint32
-	err1 := binary.Read(buf, binary.LittleEndian, &m.Frame)
-	err2 := binary.Read(buf, binary.LittleEndian, &m.Generation)
-	err3 := binary.Read(buf, binary.LittleEndian, &payloadSize)
-
-	if err := errors.Join(err1, err2, err3); err != nil {
-		return fmt.Errorf("failed to decode message header: %v", err)
+	buf := bytes.NewReader(data)
+	m.Type, err = buf.ReadByte()
+	if err != nil {
+		return err
 	}
 
-	if payloadSize > 0 {
-		m.Payload = make([]byte, payloadSize)
-		if _, err := io.ReadFull(buf, m.Payload); err != nil {
-			return fmt.Errorf("failed to decode message payload: %v", err)
+	err1 := binary.Read(buf, binary.LittleEndian, &m.Frame)
+	err2 := binary.Read(buf, binary.LittleEndian, &m.Generation)
+	err3 := binary.Read(buf, binary.LittleEndian, &dataSize)
+
+	if err = errors.Join(err1, err2, err3); err != nil {
+		return err
+	}
+
+	if dataSize > 0 {
+		m.Payload = make([]byte, dataSize)
+		if _, err = io.ReadFull(buf, m.Payload); err != nil {
+			return err
 		}
 	}
 
@@ -75,16 +82,16 @@ func readMsg(conn net.Conn) (Message, error) {
 
 	var size uint32
 	if err = binary.Read(conn, binary.LittleEndian, &size); err != nil {
-		return msg, fmt.Errorf("failed to read message length: %v", err)
+		return msg, fmt.Errorf("failed to read message length: %w", err)
 	}
 
 	buf := make([]byte, size)
 	if _, err = io.ReadFull(conn, buf); err != nil {
-		return msg, fmt.Errorf("failed to read message: %v", err)
+		return msg, fmt.Errorf("failed to read message: %w", err)
 	}
 
 	if err = msg.Decode(buf); err != nil {
-		return msg, fmt.Errorf("failed to decode message: %v", err)
+		return msg, fmt.Errorf("failed to decode message: %w", err)
 	}
 
 	return msg, nil
@@ -93,15 +100,15 @@ func readMsg(conn net.Conn) (Message, error) {
 func writeMsg(conn net.Conn, msg Message) error {
 	data, err := msg.Encode()
 	if err != nil {
-		return fmt.Errorf("failed to encode message: %v", err)
+		return fmt.Errorf("failed to encode message: %w", err)
 	}
 
 	if err = binary.Write(conn, binary.LittleEndian, uint32(len(data))); err != nil {
-		return fmt.Errorf("failed to write message length: %v", err)
+		return fmt.Errorf("failed to write message length: %w", err)
 	}
 
 	if _, err = conn.Write(data); err != nil {
-		return fmt.Errorf("failed to write message: %v", err)
+		return fmt.Errorf("failed to write message: %w", err)
 	}
 
 	return nil
