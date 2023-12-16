39a00cb7b1e12a6f1ea9713fa21a9d9204c85ce7
Max Poletaev <max.poletaev@gmail.com>
2023-12-16 23:01:03 +0100

    Missing files

---
+++ internal/binario/reader.go
@@ -0,0 +1,189 @@
+package binario
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+type Reader struct {
+	byteOrder binary.ByteOrder
+	reader    io.Reader
+	buf       []byte
+}
+
+func NewReader(reader io.Reader, byteOrder binary.ByteOrder) *Reader {
+	return &Reader{
+		reader:    reader,
+		byteOrder: byteOrder,
+		buf:       make([]byte, 8),
+	}
+}
+
+func (r *Reader) ReadBool() (bool, error) {
+	b, err := r.ReadUint8()
+	return b != 0, err
+}
+
+func (r *Reader) ReadBoolTo(dst *bool) error {
+	b, err := r.ReadBool()
+	if err != nil {
+		return err
+	}
+
+	*dst = b
+	return nil
+}
+
+func (r *Reader) ReadUint8() (uint8, error) {
+	bs := r.buf[:1]
+	if _, err := r.reader.Read(bs); err != nil {
+		return 0, err
+	}
+
+	return bs[0], nil
+}
+
+func (r *Reader) ReadUint8To(dst *uint8) error {
+	b, err := r.ReadUint8()
+	if err != nil {
+		return err
+	}
+
+	*dst = b
+	return nil
+}
+
+func (r *Reader) ReadUint16() (uint16, error) {
+	bs := r.buf[:2]
+	if _, err := r.reader.Read(bs); err != nil {
+		return 0, err
+	}
+
+	return r.byteOrder.Uint16(bs), nil
+}
+
+func (r *Reader) ReadUint16To(dst *uint16) error {
+	b, err := r.ReadUint16()
+	if err != nil {
+		return err
+	}
+
+	*dst = b
+	return nil
+}
+
+func (r *Reader) ReadUint32() (uint32, error) {
+	bs := r.buf[:4]
+	if _, err := r.reader.Read(bs); err != nil {
+		return 0, err
+	}
+
+	return r.byteOrder.Uint32(bs), nil
+}
+
+func (r *Reader) ReadUint32To(dst *uint32) error {
+	b, err := r.ReadUint32()
+	if err != nil {
+		return err
+	}
+
+	*dst = b
+	return nil
+}
+
+func (r *Reader) ReadUint64() (uint64, error) {
+	bs := r.buf[:8]
+	if _, err := r.reader.Read(bs); err != nil {
+		return 0, err
+	}
+
+	return r.byteOrder.Uint64(bs), nil
+}
+
+func (r *Reader) ReadUint64To(dst *uint64) error {
+	b, err := r.ReadUint64()
+	if err != nil {
+		return err
+	}
+
+	*dst = b
+	return nil
+}
+
+func (r *Reader) ReadBytes() ([]byte, error) {
+	length, err := r.ReadUint32()
+	if err != nil {
+		return nil, err
+	}
+
+	bs := make([]byte, length)
+	if _, err = r.reader.Read(bs); err != nil {
+		return nil, err
+	}
+
+	return bs, nil
+}
+
+func (r *Reader) ReadBytesTo(dst []byte) error {
+	length, err := r.ReadUint32()
+	if err != nil {
+		return err
+	}
+
+	if len(dst) < int(length) {
+		return io.ErrShortBuffer
+	}
+
+	bs := dst[:length]
+	if _, err = r.reader.Read(bs); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Reader) ReadString() (string, error) {
+	bs, err := r.ReadBytes()
+	return string(bs), err
+}
+
+func (r *Reader) ReadStringTo(dst *string) error {
+	bs, err := r.ReadBytes()
+	if err != nil {
+		return err
+	}
+
+	*dst = string(bs)
+	return nil
+}
+
+func (r *Reader) ReadVarUint() (uint64, error) {
+	var value uint64
+	var shift uint
+
+	for {
+		b, err := r.ReadUint8()
+		if err != nil {
+			return 0, err
+		}
+
+		value |= uint64(b&0x7F) << shift
+		if b&0x80 == 0 {
+			break
+		}
+
+		shift += 7
+	}
+
+	return value, nil
+}
+
+func (r *Reader) ReadVarUintTo(dst *uint64) error {
+	b, err := r.ReadVarUint()
+	if err != nil {
+		return err
+	}
+
+	*dst = b
+	return nil
+}
---
+++ internal/binario/writer.go
@@ -0,0 +1,96 @@
+package binario
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+// Writer is a wrapper around io.Writer that provides methods for writing binary
+// data. It is similar to the binary.Write but avoids expensive type assertions
+// by providing separate methods for each type.
+type Writer struct {
+	byteOrder binary.ByteOrder
+	writer    io.Writer
+	buf       []byte
+}
+
+// NewWriter returns a new Writer that writes to w using the specified byte order.
+func NewWriter(w io.Writer, byteOrder binary.ByteOrder) *Writer {
+	return &Writer{
+		buf:       make([]byte, 8),
+		byteOrder: byteOrder,
+		writer:    w,
+	}
+}
+
+func (w *Writer) WriteBool(value bool) error {
+	if value {
+		return w.WriteUint8(1)
+	}
+
+	return w.WriteUint8(0)
+}
+
+// WriteUint8 writes a single byte.
+func (w *Writer) WriteUint8(value uint8) error {
+	_, err := w.writer.Write([]byte{value})
+	return err
+}
+
+// WriteUint16 writes a 16-bit unsigned integer.
+func (w *Writer) WriteUint16(value uint16) error {
+	bf := w.buf[:2]
+	w.byteOrder.PutUint16(bf, value)
+	_, err := w.writer.Write(bf)
+
+	return err
+}
+
+// WriteUint32 writes a 32-bit unsigned integer.
+func (w *Writer) WriteUint32(value uint32) error {
+	bf := w.buf[:4]
+	w.byteOrder.PutUint32(bf, value)
+	_, err := w.writer.Write(bf)
+
+	return err
+}
+
+// WriteUint64 writes a 64-bit unsigned integer.
+func (w *Writer) WriteUint64(value uint64) error {
+	bf := w.buf[:8]
+	w.byteOrder.PutUint64(bf, value)
+	_, err := w.writer.Write(bf)
+
+	return err
+}
+
+// WriteBytes writes a byte slice prefixed with its length.
+func (w *Writer) WriteBytes(value []byte) error {
+	length := uint32(len(value))
+	if err := w.WriteUint32(length); err != nil {
+		return err
+	}
+
+	_, err := w.writer.Write(value)
+
+	return err
+}
+
+// WriteString writes a string prefixed with its length.
+func (w *Writer) WriteString(value string) error {
+	return w.WriteBytes([]byte(value))
+}
+
+// WriteVarUint writes a variable-length encoded unsigned integer.
+// See https://developers.google.com/protocol-buffers/docs/encoding#varints
+func (w *Writer) WriteVarUint(value uint64) error {
+	for value >= 0x80 {
+		if err := w.WriteUint8(uint8(value) | 0x80); err != nil {
+			return err
+		}
+
+		value >>= 7
+	}
+
+	return w.WriteUint8(uint8(value))
+}
